6b0b1661215def418434454cfd5b59f461758caa
samyak-10 <ksamyak698@gmail.com>
2026-08-26 16:27:56 +0530

    initializeing Project

---
+++ src/main/java/com/equipment/event_equipment_booking/EventEquipmentBookingApplication.java
@@ -0,0 +1,13 @@
+package com.equipment.event_equipment_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEquipmentBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEquipmentBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipment/event_equipment_booking/EventEquipmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipment.event_equipment_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEquipmentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
